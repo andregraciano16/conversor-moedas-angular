50294e546d66ccb3516cc0ab3a1954ef1b2b2777
andregraciano16 <andregraciano16@gmail.com>
2020-07-19 01:09:09 -0300

    adicionando componentes e services de moeda e conversor

---
+++ src/app/conversor/services/moeda.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { Moeda } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoedaService {
+
+  private moedas: Moeda[];
+
+  constructor() { }
+
+  private moedasObj = [
+    { "sigla": 'AUD', 'descricao': 'Dólar australiano' },
+    { "sigla": 'BGN', 'descricao': 'Lev búlgaro' },
+    { "sigla": 'BRL', 'descricao': 'Real Brasileiro' },
+    { "sigla": 'CAD', 'descricao': 'Dólar australiano' },
+    { "sigla": 'CHF', 'descricao': 'Franco suíço' }
+  ];
+
+  listarTodas(): Moeda[] {
+    if (this.moedas) {
+      return this.moedas;
+    }
+
+    this.moedas = [];
+
+    for (const moedaObj of this.moedasObj) {
+      const moeda: Moeda = new Moeda();
+      Object.assign(moeda, moedaObj);
+      this.moedas.push(moeda);
+    }
+  }
+}
